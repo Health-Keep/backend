853f95e637503cc1094f2999ce45aad0c76603c4
DGreegman <obeagug@gmail.com>
2024-06-14 23:20:14 +0100

    Added files into the user interface for patience

--- src/features/users/interfaces/user-interface.ts
+++ src/features/users/interfaces/user-interface.ts
@@ -1,2 +1,12 @@
 import { Document } from "mongoose";
 
+// interface for the patient 
+interface IUser extends Document {
+    name: string;
+    email: string;
+    gender: string;
+    nin: number;
+    password: string;
+}
+
+export default IUser;
