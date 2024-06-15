c51cb18aca0832b8f65bdfc411f8c0cef7f0ebf7
Heba-Webdev23 <jtvonline@yahoo.com>
2024-06-15 15:27:09 +0300

    fix: added enum for gender

--- src/features/users/models/user-model.ts
+++ src/features/users/models/user-model.ts
@@ -4,7 +4,7 @@ import IUser from "../interfaces/user-interface";
 const UserSchema = new Schema({
     name: { type: String, required: true },
     email: { type: String, required: true, unique: true },
-    gender: { type: String, required: true },
+    gender: { type: String, required: true, enum: ["Male", "Female"] },
     nin: { type: Number, required: true, unique: true },
     password: { type: String, required: true },
 })
