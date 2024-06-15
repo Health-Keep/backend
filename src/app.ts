ded5d51f4b9e4420999681fe6f5ca27ae9338b61
Heba-Webdev23 <jtvonline@yahoo.com>
2024-06-15 15:24:00 +0300

    feat: added routes for api-docs and users

--- src/app.ts
+++ src/app.ts
@@ -1,19 +1,27 @@
-import express from "express";
+import express, { json } from "express";
 import cors from 'cors';
+import config from "./docs/swagger-output.json";
 import cookieParser from "cookie-parser";
 import swaggerUi from 'swagger-ui-express';
 import { errorHandler } from "./middleware/error.middleware";
 import { notFound } from "./middleware/notFound.middleware";
+import connect_db from "./config/mongo";
+import { userRouter } from "./features/users/routes";
 
 
 const app = express();
 const PORT = process.env.PORT || 5002;
 
 app.use(cors());
+app.use(express.json());
+
+app.use('/api-docs', swaggerUi.serve, swaggerUi.setup(config));
+app.use("/api/users", userRouter);
 
 app.use(errorHandler);
 app.all("*", notFound);
 
 app.listen(PORT, () => {
+    connect_db();
     console.log(`Server running on port ${PORT}`);
 });
