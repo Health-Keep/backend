fc3186ca44ae856f81f6b59af0d11879e5e5bb0c
Heba-Webdev23 <jtvonline@yahoo.com>
2024-06-16 03:03:52 +0300

    feat: user's login route

--- src/features/users/routes/index.ts
+++ src/features/users/routes/index.ts
@@ -1,11 +1,14 @@
 import { Router } from "express";
 // Controllers
 import { signup } from "../controllers/signup.users";
+import { signin } from "../controllers/signin.users";
 // Input's validation
 import { signupValidation } from "../input-validation/signup.validation";
+import { signinValidation } from "../input-validation/signin.validation";
 
 const userRouter = Router()
 
 userRouter.route("/signup").post(signupValidation, signup);
+userRouter.route("/signin").post(signinValidation, signin);
 
 export { userRouter }
