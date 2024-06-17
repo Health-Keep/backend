796ed1984bfcd5afc1dba8f9c2ce05341e042a66
Heba-Webdev23 <jtvonline@yahoo.com>
2024-06-17 23:00:04 +0300

    fix: org router

--- src/app.ts
+++ src/app.ts
@@ -1,14 +1,12 @@
-import express, { json } from "express";
+import express from 'express';
 import cors from 'cors';
-import config from "./docs/swagger-output.json";
-import cookieParser from "cookie-parser";
+import config from './docs/swagger-output.json';
 import swaggerUi from 'swagger-ui-express';
-import { errorHandler } from "./middleware/error.middleware";
-import { notFound } from "./middleware/notFound.middleware";
-import connect_db from "./config/mongo";
-import { userRouter } from "./features/users/routes";
-import org_router from "./features/organizations/routes/";
-
+import { errorHandler } from './middleware/error.middleware';
+import { notFound } from './middleware/notFound.middleware';
+import connect_db from './config/mongo';
+import { user_router } from './features/users/routes';
+import { org_router } from './features/organizations/routes/';
 
 const app = express();
 const PORT = process.env.PORT || 5002;
@@ -17,11 +15,11 @@ app.use(cors());
 app.use(express.json());
 
 app.use('/api-docs', swaggerUi.serve, swaggerUi.setup(config));
-app.use("/api/users", userRouter);
-app.use("api/org/", org_router);
+app.use('/api/users', user_router);
+app.use('/api/org', org_router);
 
 app.use(errorHandler);
-app.all("*", notFound);
+app.all('*', notFound);
 
 app.listen(PORT, () => {
     connect_db();
